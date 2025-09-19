6f52524df3547615d1d7b28cdc920d4593922144
HossahinDev <hossahin321@gmail.com>
2025-09-20 02:08:07 +0600

    check anather commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,8 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
       <p>Get started by editing <code>app/page.js</code></p>
       <p className="mt-4 text-lg">This is a sample Next.js application.</p>
+
+      <p className="mt-4 text-lg">You can add more content here.</p>
     </div>
   );
 }
