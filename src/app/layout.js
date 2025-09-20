51e22bd9390921b56437836b121da9acb0d0e3cd
HossahinDev <hossahin321@gmail.com>
2025-09-20 23:38:02 +0600

    implement basic navbar layout with links and icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,10 +19,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  max-w-screen-2xl mx-auto`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="mt-12">
       <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
       <p>Get started by editing <code>app/page.js</code></p>
       <p className="mt-4 text-lg">This is a sample Next.js application.</p>
