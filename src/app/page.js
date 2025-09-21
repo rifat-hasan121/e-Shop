4bbce242c3644f097a82d1c0ff6cd3da97e2297e
HossahinDev <hossahin321@gmail.com>
2025-09-21 14:18:06 +0600

    implement product highlight section on Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import BrowseByCategory from "./components/BrowseByCategory";
+import Highlight from "./components/Highlight";
 
 export default function Home() {
   return (
     <div className="mt-10">
       <BrowseByCategory />
+      <Highlight />
     </div>
   );
 }
