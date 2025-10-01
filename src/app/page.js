e74b2c552c9fd55caf02c85065dc1de031283c67
Hossahin <hossahin321@gmail.com>
2025-10-02 01:20:39 +0600

    check commit

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ import WhyChooseUs from "./components/WhyChooseUs";
 export default function Home() {
   return (
     <div className="mt-10">
+      <p className="text-2xl font-bold text-center">
+        Welcome to e-Shop
+      </p>
       <BrowseByCategory />
       <Highlight />
       <WhyChooseUs />
